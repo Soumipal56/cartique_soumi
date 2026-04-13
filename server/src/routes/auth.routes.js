73d60b62622500b2d6092faa0bb202f323b80cc9
Soumipal56 <soumiisc2020@gmail.com>
2026-04-13 10:01:51 +0530

    done continue with google feature

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { validateRegisterUser, validateLoginUser } from "../validator/auth.validator.js";
-import { register, login } from "../controllers/auth.controller.js";
+import { googleCallback, register, login } from "../controllers/auth.controller.js";
+import passport from "passport";
+import config from "../config/config.js";
 
 const router = Router();
 
@@ -8,5 +10,14 @@ const router = Router();
 router.post('/register', validateRegisterUser, register)
 
 router.post("/login", validateLoginUser, login)
+
+// /api/auth/google
+router.get("/google",
+    passport.authenticate("google", { scope: [ "profile", "email" ] }))
+
+router.get("/google/callback",
+    passport.authenticate("google", { session: false, failureRedirect: config.NODE_ENV === "development" ? "http://localhost:5173/login" : "/login" }),
+    googleCallback,
+)
 
 export default router;
