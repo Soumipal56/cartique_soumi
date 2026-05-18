0e9ed6a2a27826e5f4e4a3b420340f5b64095ef6
Soumipal56 <soumiisc2020@gmail.com>
2026-05-18 09:34:50 +0530

    config file done

---
+++ Backend/src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+
+const app = express();
+
+// Global Middlewares
+app.use(morgan('dev')); // HTTP request logger
+app.use(express.json()); // Body parser for JSON
+app.use(express.urlencoded({ extended: true })); // Body parser for URL-encoded data
+app.use(cookieParser()); // Cookie parser middleware
+
+// Base API route for healthcheck/testing
+app.get("/", (_req, res) => {
+    res.status(200).json({ message: "Server is running" });
+});
+
+export default app;
