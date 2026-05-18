10403ef6e38e87462029e5a75803a0aa80a6ada8
Soumipal56 <soumiisc2020@gmail.com>
2026-05-18 12:23:06 +0530

    feat: implement user registration controller and auth route with JWT authentication

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -2,11 +2,27 @@ import userModel from "../models/user.model.js";
 import jwt from "jsonwebtoken"
 import { config } from "../config/config.js";
 
-async function sendTokenResponse(user, res){
+async function sendTokenResponse(user, res, message){
 
     const token = jwt.sign({
         id: user._id,       
-    }, config.JWT_SECRET)
+    }, config.JWT_SECRET, {
+        expiresIn: "7d"
+    })
+
+    res.cookie("token", token)
+
+    res.status(200).json({
+        message,
+        success: true,
+        user: {
+            id: user._id,
+            email: user.email,
+            contact: user.contact,
+            fullname: user.fullname,
+            role: user.role
+        }
+    })
 }
 
 export const register = async (req, res) => {
@@ -34,6 +50,8 @@ export const register = async (req, res) => {
             password,
             fullname
         })
+
+        await sendTokenResponse(user, res, "User registered successfully")
 
         
     } catch (error) {
