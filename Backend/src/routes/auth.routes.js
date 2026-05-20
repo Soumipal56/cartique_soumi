a78241016015a4160997e490863a86ea92b552fa
Soumipal56 <soumiisc2020@gmail.com>
2026-05-20 10:49:23 +0530

    feat: implement user authentication controllers and routes for registration, login, and Google OAuth callback

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { validateRegisterUser, validateLoginUser } from "../validator/auth.validator.js";
-import { register, login } from "../controllers/auth.controller.js";
+import { register, login, googleCallback } from "../controllers/auth.controller.js";
 import passport from "passport";
 
 const router = Router();
@@ -9,12 +9,12 @@ router.post('/register', validateRegisterUser, register)
 
 router.post("/login", validateLoginUser, login)
 
-router.get("/auth/google", passport.authenticate("google", {
+router.get("/google", passport.authenticate("google", {
     scope: ["profile", "email"]
 }))
 
-router.get("/auth/google/callback", passport.authenticate("google", {
+router.get("/google/callback", passport.authenticate("google", {
     session: false
-}), )
+}), googleCallback)
 
 export default router
