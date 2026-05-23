4649db9dd2cc83ae5eebfc9ab2ca4ac97d6e7395
Soumipal56 <soumiisc2020@gmail.com>
2026-05-23 13:35:03 +0530

    feat: implement seller product retrieval and related state management

--- Backend/src/routes/product.routes.js
+++ Backend/src/routes/product.routes.js
@@ -3,6 +3,7 @@ import { authenticateSeller } from "../middleware/auth.middleware.js";
 import { createProduct } from "../controllers/product.controller.js";
 import multer from "multer";
 import { createProductValidator } from "../validator/product.validator.js";
+import { getSellerProducts } from "../controllers/product.controller.js";
 
 const upload = multer({
     storage: multer.memoryStorage(),
@@ -13,6 +14,18 @@ const upload = multer({
 
 const router = Router();
 
+/**
+ * @route POST /api/products
+ * @desc Create a new product
+ * @access Private (Seller only)
+ */
 router.post("/", authenticateSeller, createProductValidator, upload.array("images", 7), createProduct)
+
+/**
+ * @route GET /api/products/seller
+ * @desc Get products of the authenticated seller
+ * @access Private (Seller only)
+ */
+router.get("/seller", authenticateSeller, getSellerProducts)
 
 export default router;
---
+++ Frontend/src/features/products/hooks/useProduct.jsx
@@ -0,0 +1,23 @@
+import { createProduct, getSellerProduct } from "../service/product.api";
+import { setSellerProducts } from "../state/product.slice";
+import { useDispatch } from "react-redux";
+
+export const useProduct = () => {
+    const dispatch = useDispatch()
+
+
+    async function handleCreateProduct(formData) {
+
+        const data = await createProduct(formData)
+        return data.product
+    }
+
+    async function handleGetSellerProduct() {
+
+        const data = await getSellerProduct()
+        dispatch(setSellerProducts(data.products))
+        return data.products
+    }
+
+    return { handleCreateProduct, handleGetSellerProduct }
+}
