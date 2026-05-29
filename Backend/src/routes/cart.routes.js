a11e7023984ae1d0bae8357689287de479c090f3
Soumipal56 <soumiisc2020@gmail.com>
2026-05-29 22:20:55 +0530

    feat: implement cart management system including DAO, routes, validation, and controller logic

---
+++ Backend/src/routes/cart.routes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { authenticateUser } from "../middleware/auth.middleware.js";
+import { validateAddToCart } from "../validator/cart.validator.js";
+import { addToCart } from "../controllers/cart.controller.js";
+
+const router = express.Router();
+
+/**
+ * @route POST /api/cart/add/:productId/:variantId
+ * @desc Add item to cart
+ * @access Private
+ * @argument productId - ID of the product to add
+ * @argument variantId - ID of the variat to add
+ * @argument quantity - Quantity of the item to add (optional, default: 1)
+ */
+router.post("/add/:productId/:variantId", authenticateUser, validateAddToCart, addToCart)
+
+export default router;
