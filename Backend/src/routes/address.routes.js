439144fa32b32c2251b121aae3cee42bec42d72d
Soumipal56 <soumiisc2020@gmail.com>
2026-06-18 17:37:18 +0530

    fix: correct typo in middleware import path

--- Backend/src/routes/address.routes.js
+++ Backend/src/routes/address.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { addAddress, getUserAddresses } from "../controllers/address.controller.js";
-import { isAuth } from "../middlewares/auth.middleware.js";
+import { isAuth } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
